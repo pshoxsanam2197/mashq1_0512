87ec2e8159fe3cc94a378afcf205f9ccb68740ea
pshoxsanam2197 <pshoxsanam2197@gmail.com>
2025-12-05 15:08:21 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#1-misol
+for i in range(3):
+    print(" $ $ $ $")
+
+# 2-misol
+for i in range(1, 5):
+    for n in range(i):
+        print("0 ", end="")
+    print()
+
+# 3-misol
+for i in [2, 3, 4, 3, 2]:
+    print("@ " * i)
+
+# 4-misol
+for i in range(1, 5):
+    print((str(i) + " ")* i +"#")
